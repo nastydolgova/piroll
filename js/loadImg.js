8a9ead03149345fb71164e937f89dfe5c3050f27
Anastasia D <nasty.dolgova@gmail.com>
2020-11-01 21:06:11 +0300

    1

--- js/loadImg.js
+++ js/loadImg.js
@@ -3,7 +3,7 @@
 (function () {
     let loadButton = document.querySelector('.button-load');
     let gallery = document.querySelector('.gallery');
-    let IMAGES=['/img/5.jpg','/img/6.jpg','/img/7.jpg','/img/8.jpg'];
+    let IMAGES=['./img/5.jpg','./img/6.jpg','./img/7.jpg','./img/8.jpg'];
     loadButton.addEventListener('click', function(){
         loadButton.classList.add('hidden');
         IMAGES.forEach(function (el){
